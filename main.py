dcf64b3063c7a2af2d6ce3aafa01e2f87ffbe3b3
Elouen <138658525+Yoshkacafe@users.noreply.github.com>
2023-11-10 12:06:14 +0100

    Update main.py
    Première update

--- main.py
+++ main.py
@@ -8,26 +8,50 @@ BLACK = (0, 0, 0)
 
 def main():
         pygame.init()
-        cell_size = 75
-        num_rows = 14
-        num_cols = 26
+        cell_size = 30
+        num_rows = 25
+        num_cols = 25
         screen_width = cell_size * num_cols
         screen_height = cell_size * num_rows
         screen = pygame.display.set_mode((screen_width, screen_height))
         pygame.display.set_caption("Labyrinthe")
         map = Map(num_cols, num_rows)
         map.generate()
-        player = Player(0, 0)
         running = True
 
         while running:
           for event in pygame.event.get():
             if event.type == pygame.QUIT:
               running = False
-          # Ici, vous pouvez gérer les événements de déplacement du joueur
+
           screen.fill(WHITE)
+
+          # Affichage de la map grace à Map.py
+
           map.draw(screen, cell_size)
-          player.draw(screen, cell_size)
+
+          # Initialisation du joueur grace à Player.py
+
+          p = Player(screen, screen_height, 0)
+          p.draw()
+
+          # Déplacement vers le haut, bas, gauche, droite du joueur définit précedement
+
+          keys = pygame.key.get_pressed()
+          if keys[pygame.K_UP]:
+              p.top()
+              pygame.display.update()
+          elif keys[pygame.K_DOWN]:
+              p.down()
+              pygame.display.update()
+          elif keys[pygame.K_RIGHT]:
+              p.right()
+              pygame.display.update()
+          elif keys[pygame.K_LEFT]:
+              p.left()
+              pygame.display.update()
+              
+          p.direction() # Faire un if pour chaque direction et vérifier dans le main
           pygame.display.flip()
         pygame.quit()
 
