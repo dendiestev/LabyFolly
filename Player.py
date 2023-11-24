7f6a5fec19c6ad799c0ca211e4b42369001b1079
Elouen <138658525+Yoshkacafe@users.noreply.github.com>
2023-11-24 12:08:46 +0100

    Update Player.py

--- Player.py
+++ Player.py
@@ -10,55 +10,27 @@ class Player():
     """
     
     def __init__(self, screen, position_x, position_y):
+        self.screen = screen
         self.position_x = position_x
         self.position_y = position_y
-        self.screen = screen
 
     def draw(self):
-        position_x = 10
-        position_y = 10
-        width = 10
-        height = 10
-        pygame.draw.rect(self.screen, (255,0,0), (position_x, position_y, width, height))
+        width = 20
+        height = 20
+        pygame.draw.rect(self.screen, (255,255,255), (self.position_x, self.position_y, width, height))
 
     def top(self):
-        self.position_y -= self.vel
+        self.position_y -= 5
+        print(self.position_y)
 
     def down(self):
-        self.position_y += self.vel
+        self.position_y += 5
+        print(self.position_y)
 
     def right(self):
-        self.position_x += self.vel
+        self.position_x += 5
+        print(self.position_x)
     
     def left(self):
-        self.position_x -= self.vel
-
-    def direction(self):
-        x = 50
-        y = 50
-        width = 10
-        height = 10
-        vel = 10
-
-        pygame.time.delay(100)
-        
-    
-
-# class Player:
-
-#     ### Initialisation ###
-#     def __init__(self, row, col):
-#         self.row = row
-#         self.col = col
-
-#     ### Mouvements ###
-
-#     def move(self, dx, dy):
-#         # Ici, vous pouvez mettre votre logique de déplacement du joueur
-#         pass
-
-#     ### Skin Du Joueur ###
-
-#     def draw(self, screen):
-#         # Ici, vous pouvez dessiner le joueur
-#         pass
+        self.position_x -= 5
+        print(self.position_x)
