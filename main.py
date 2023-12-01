01af7c3d1eec1e5e613c0df15b3e1b2442c64a18
Elouen <138658525+Yoshkacafe@users.noreply.github.com>
2023-12-01 10:43:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,76 @@
+import pygame
 
+from Map import *
+from Player import *
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+def main():
+        pygame.init()
+        cell_size = 30
+        num_rows = 25
+        num_cols = 25
+        screen_width = cell_size * num_cols
+        screen_height = cell_size * num_rows
+        screen = pygame.display.set_mode((screen_width, screen_height))
+        pygame.display.set_caption("Labyrinthe")
+        map = Map(num_cols, num_rows)
+        map.generate()
+
+        # Initialisation du joueur grace à Player.py
+
+        p = Player(screen, 20, 20)
+
+        running = True
+
+        while running:
+          for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+              running = False
+
+          screen.fill(WHITE)
+
+          # Affichage de la map grace à Map.py
+
+          map.draw(screen, screen_width, screen_height)
+          
+          # Affichage du player
+
+          p.draw()
+
+          # Déplacement vers le haut, bas, gauche, droite du joueur définit précedement
+
+          keys = pygame.key.get_pressed()
+          if keys[pygame.K_UP]:
+              p.top()
+              p.draw()
+              pygame.display.update()
+          elif keys[pygame.K_DOWN]:
+              p.down()
+              p.draw()
+              pygame.display.update()
+          elif keys[pygame.K_RIGHT]:
+              p.right()
+              p.draw()
+              pygame.display.update()
+          elif keys[pygame.K_LEFT]:
+              p.left()
+              p.draw()
+              pygame.display.update()
+          
+          # Vérification pour les bors de la map
+
+          if p.position_x <= 0:
+              p.position_x = 0
+          if p.position_x >= screen_width - 20:
+              p.position_x = screen_width - 20
+          if p.position_y <= 0:
+              p.position_y = 0
+          if p.position_y >= screen_height - 20:
+              p.position_y = screen_height - 20
+          pygame.display.flip()
+        pygame.quit()
+
+if __name__ == "__main__":
+    main()
