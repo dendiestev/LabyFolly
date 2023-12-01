bc7121fc5456539e1a61d7495a5753527bb773db
Elouen <138658525+Yoshkacafe@users.noreply.github.com>
2023-12-01 12:04:45 +0100

    Update Player.py

--- Player.py
+++ Player.py
@@ -3,6 +3,7 @@ import os
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED_LIGHT = (245, 66, 66)
 
 class Player():
     """
@@ -17,20 +18,16 @@ class Player():
     def draw(self):
         width = 20
         height = 20
-        pygame.draw.rect(self.screen, (255,255,255), (self.position_x, self.position_y, width, height))
+        pygame.draw.rect(self.screen, RED_LIGHT, (self.position_x, self.position_y, width, height))
 
     def top(self):
         self.position_y -= 5
-        print(self.position_y)
 
     def down(self):
         self.position_y += 5
-        print(self.position_y)
 
     def right(self):
         self.position_x += 5
-        print(self.position_x)
     
     def left(self):
         self.position_x -= 5
-        print(self.position_x)
