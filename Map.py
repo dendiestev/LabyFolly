dd19856263883cfbbc6baf5dd93b23249b6d5b92
Elouen <138658525+Yoshkacafe@users.noreply.github.com>
2023-12-01 12:04:20 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -3,95 +3,51 @@ import random
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED_LIGHT = (245, 66, 66)
 
 class Map:
 
-    ### Création ###
-
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.cells = [[False] * width for _ in range(height)]
-        self.walls = []  # Liste des murs
-
-        # Initialisez les murs et les cellules
-        for row in range(height):
-            for col in range(width):
-                if row % 2 == 0 and col % 2 == 0:
-                    continue
-                self.cells[row][col] = True
-
-                # Ajoutez les murs verticaux
-                if col > 0:
-                    self.walls.append(((row, col), (row, col - 1)))
-                if col < width - 1:
-                    self.walls.append(((row, col), (row, col + 1)))
-
-                # Ajoutez les murs horizontaux
-                if row > 0:
-                    self.walls.append(((row, col), (row - 1, col)))
-                if row < height - 1:
-                    self.walls.append(((row, col), (row + 1, col)))
-
-        random.shuffle(self.walls)  # Mélangez la liste des murs
-
-    
-
-    def find_set(self, sets, cell):
-        for i, s in enumerate(sets):
-            if cell in s:
-                return i
-        return None
-
-
-
-    """
-    ### Génaration ###
-
-    def generate(self):
-        sets = [{(row, col)} for row in range(self.height) for col in range(self.width)]
-
-        for wall in self.walls:
-            (cell1, cell2) = wall
-            set1_index = self.find_set(sets, cell1)
-            set2_index = self.find_set(sets, cell2)
-
-            if set1_index != set2_index:
-                self.cells[cell1[0]][cell1[1]] = False
-                sets[set1_index] |= sets[set2_index]
-                sets.pop(set2_index)
-
-    def generate(self):
-    # Ici, vous pouvez mettre votre logique de génération de labyrinthe
-        pass
-    """
-    
-    def find_set(self, sets, cell):
-        for i, s in enumerate(sets):
-            if cell in s:
-                return i
-        return None
-
-    def generate(self):
-        sets = [{(row, col)} for row in range(self.height) for col in range(self.width)]
-
-        for wall in self.walls:
-            (cell1, cell2) = wall
-            (row1, col1), (row2, col2) = cell1, cell2
-            set1_index = self.find_set(sets, (row1, col1))
-            set2_index = self.find_set(sets, (row2, col2))
-
-            if set1_index != set2_index:
-                self.cells[row1][col1] = False
-                sets[set1_index] |= sets[set2_index]
-                sets.pop(set2_index)
-
-
     ### Dessin ###
-    def draw(self, screen, screen_width, screen_height): 
-        pygame.draw.rect(screen, BLACK, (screen_width - 50, screen_height - 50, 50, 50))
+
+    def draw(self, screen, screen_width, screen_height):
+        num_col = screen_height // 30
+        print(num_col)
+        collision_list = []
+        for i in range(num_col):
+            pygame.draw.rect(screen, BLACK, (screen_width - 30, screen_height - 30*i, 30, 30))
+            mur_col_pos = (screen_width - 30, screen_height - 30*i)
+            collision_list.append(mur_col_pos)
+        print(collision_list)
+
+
     # def draw(self, screen, cell_size):
     #     for row in range(self.height):
     #         for col in range(self.width):
     #             if self.cells[row][col]:
     #                 pygame.draw.rect(screen, BLACK, (col * cell_size, row * cell_size, cell_size, cell_size))
+
+    # def find_set(self, sets, cell):
+    #     for i, s in enumerate(sets):
+    #         if cell in s:
+    #             return i
+    #     return None
+    
+    # def find_set(self, sets, cell):
+    #     for i, s in enumerate(sets):
+    #         if cell in s:
+    #             return i
+    #     return None
+
+    # def generate(self):
+    #     sets = [{(row, col)} for row in range(self.height) for col in range(self.width)]
+
+    #     for wall in self.walls:
+    #         (cell1, cell2) = wall
+    #         (row1, col1), (row2, col2) = cell1, cell2
+    #         set1_index = self.find_set(sets, (row1, col1))
+    #         set2_index = self.find_set(sets, (row2, col2))
+
+    #         if set1_index != set2_index:
+    #             self.cells[row1][col1] = False
+    #             sets[set1_index] |= sets[set2_index]
+    #             sets.pop(set2_index)
