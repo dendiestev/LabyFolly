3fa760485f18ed140fe20e5eab3897df03e64bd8
Elouen <138658525+Yoshkacafe@users.noreply.github.com>
2023-12-08 06:53:37 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -7,16 +7,51 @@ RED_LIGHT = (245, 66, 66)
 
 class Map:
 
+    ### Initialisation ###
+
+    def __init__(self, collision_list=[]):
+        self.collision_list = collision_list
+
     ### Dessin ###
 
-    def __init__(self) -> None:
-        self.collision_list = []
-
     def draw(self, screen, screen_width, screen_height):
-        # Ensuite il faut nommé avec X, y, 1(x, y), 1(y, x), 2(x, y), 2(y, x)
         num_col = screen_height // 30
+        collision_list = []
         for i in range(num_col):
             pygame.draw.rect(screen, BLACK, (screen_width - 30, screen_height - 30*i, 30, 30))
-            mur_col_pos = [screen_width - 30, screen_height - 30*i]
-            self.collision_list.append(mur_col_pos)
-        return(self.collision_list)
+            mur_col_pos = (screen_width - 30, screen_height - 30*i)
+            collision_list.append(mur_col_pos)
+        print(collision_list)
+
+
+    # def draw(self, screen, cell_size):
+    #     for row in range(self.height):
+    #         for col in range(self.width):
+    #             if self.cells[row][col]:
+    #                 pygame.draw.rect(screen, BLACK, (col * cell_size, row * cell_size, cell_size, cell_size))
+
+    # def find_set(self, sets, cell):
+    #     for i, s in enumerate(sets):
+    #         if cell in s:
+    #             return i
+    #     return None
+    
+    # def find_set(self, sets, cell):
+    #     for i, s in enumerate(sets):
+    #         if cell in s:
+    #             return i
+    #     return None
+
+    # def generate(self):
+    #     sets = [{(row, col)} for row in range(self.height) for col in range(self.width)]
+
+    #     for wall in self.walls:
+    #         (cell1, cell2) = wall
+    #         (row1, col1), (row2, col2) = cell1, cell2
+    #         set1_index = self.find_set(sets, (row1, col1))
+    #         set2_index = self.find_set(sets, (row2, col2))
+
+    #         if set1_index != set2_index:
+    #             self.cells[row1][col1] = False
+    #             sets[set1_index] |= sets[set2_index]
+    #             sets.pop(set2_index)
