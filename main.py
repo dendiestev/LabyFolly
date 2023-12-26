f2dc97b10131704a7a6709223abfe6ca1ff73aab
Elouen <138658525+Yoshkacafe@users.noreply.github.com>
2023-12-26 16:40:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,129 +1,94 @@
 import pygame
-import time
 
 from Map import *
 from Player import *
-from Button import *
-from Leaderboard import *
-from NewGame import *
-
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-
-
 def main():
-    # pygame setup
-    screen_etat = 0
-    etat = "menu"
-    pygame.init()
-    screen = pygame.display.set_mode((1050, 750))
-    clock = pygame.time.Clock()
-    running = True
-    bleaderborad = Button(screen=screen, pos=(440,250), path_img_down="assets/buttons/leaderboard/down.png", path_img_up="assets/buttons/leaderboard/up.png")
-    bnew = Button(screen=screen, pos=(440,320), path_img_down="assets/buttons/new/down.png", path_img_up="assets/buttons/new/up.png")
-    bload = Button(screen=screen, pos=(440,390), path_img_down="assets/buttons/load/down.png", path_img_up="assets/buttons/load/up.png")
-    bback = Button(screen=screen, pos=(465,650), path_img_down="assets/buttons/go_back/down.png", path_img_up="assets/buttons/go_back/up.png")
-    l = Leaderboar(screen=screen)
-    newgame = NewGame(screen)
+        pygame.init()
+        cell_size = 30
+        num_rows = 25
+        num_cols = 35
+        screen_width = 21*30 #taille de chemin -> 20 * 30 (le nombre de case de chemin + 5 (taille de mur) * 29 (nombre de mur)
+        screen_height = 21*30
+        screen = pygame.display.set_mode((screen_width, screen_height))
+        pygame.display.set_caption("Labyrinthe")
+        map = Map(screen, screen_width, screen_height)
+        map.generer_matrice()
+        map.matrice_finale()
+        map.afficher2pointzero()
+        # Initialisation du joueur grace à Player.py
+        # Set la velocite du joueur proportionnellement à la taille de la fenêtre
+        p = Player(screen, screen_width - 52.5, screen_height - 52.5, .1)
+        map.set_collider(p.player)
 
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    if etat == "menu":
-                        if bleaderborad.over():
-                            etat = "leaderboard"
-                        if bnew.over():
-                            etat = "new game"
-                        if bload.over():
-                            etat = "load game"
-                    if etat == "leaderboard":
-                        if l.breload.over():
-                            l.reload()
-                    if etat == "new game":
-                        if newgame.baright.over():
-                            if newgame.perso < len(newgame.liste_perso)-1:
-                                newgame.perso +=1
-                            else:
-                                newgame.perso = 0
-                        if newgame.baleft.over():
-                            if newgame.perso > 0:
-                                newgame.perso -= 1
-                            else:
-                                newgame.perso = len(newgame.liste_perso)-1
-                        if newgame.bmapcod.over():
-                            newgame.map = 0
-                        if newgame.bmapftn.over():
-                            newgame.map = 1
-                        if newgame.bmaphp.over():
-                            newgame.map = 2
-                        if newgame.bmapmk.over():
-                            newgame.map = 3
-                        if newgame.bmapvalo.over():
-                            newgame.map = 4
-                        if newgame.bstart.over():
-                            check = newgame.check()
-                            print(newgame.error)
-                            print(check)
-                            print(newgame.cooldown)
-                            print(newgame.error)
-                            if check == True:
-                                newgame.start()
-                                etat = "game"
-                if etat in ["new game","leaderboard"]:             
-                    if bback.over():
-                        etat = "menu"
-            if etat == "new game":
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if newgame.input_rect.collidepoint(event.pos):
-                        newgame.active = True
-                    else:
-                        newgame.active = False
+        screen.fill(WHITE)
+
+        # Affichage de la map grace à Map.py
+
+        map.afficher_graphique()
+        map.spawn_enemies(15, 15)
+        map.afficher_enemies()
+                
+        # Affichage du player
+
+        p.draw()
         
-                if event.type == pygame.KEYDOWN:
-        
-                    if newgame.active:
-                        if event.key == pygame.K_BACKSPACE:
-                            newgame.user_text = newgame.user_text[:-1]
+        running = True
 
-                        elif len(newgame.user_text)+2 > 15:
-                            newgame.user_text = "Max 15 charactere"
-                        else:
-                            
-                            newgame.user_text += event.unicode
-        # to show buttons created
-        if etat == "menu":
-            screen.fill("purple")
-            logo = pygame.image.load("assets/logo/main.png").convert()
-            screen.blit(logo, (140, 50))
-            bleaderborad.update()
-            bload.update()
-            bnew.update()
-        if etat == "leaderboard":
-            screen.fill("blue")
-            l.draw_leaderboard()
-            bback.update()
-        if etat == "new game":
-            screen.fill("red")
-            newgame.update()
-            bback.update()
-        if time.time() < newgame.cooldown+1:
-            newgame.affiche_error(newgame.error)
-        if etat == "load game":
-            screen.fill("green")
-            bback.update()
-        if etat == "game":
-            screen.fill("yellow")
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
 
-        pygame.display.flip()
-        pygame.display.update()
-        clock.tick(60)
 
-pygame.quit()
+            # Déplacement vers le haut, bas, gauche, droite du joueur définit précedement
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_UP]:
+                p.clear_player()
+                p.top()
+                p.draw()
+                pygame.display.update()
+            elif keys[pygame.K_DOWN]:
+                p.clear_player()
+                p.down()
+                p.draw()
+                pygame.display.update()
+            elif keys[pygame.K_RIGHT]:
+                p.clear_player()
+                p.right()
+                p.draw()
+                pygame.display.update()
+            elif keys[pygame.K_LEFT]:
+                p.clear_player()
+                p.left()
+                p.draw()
+                pygame.display.update()
+            
+            # Vérification pour les bors de la map
+
+            if p.position_x <= 0:
+                p.position_x = 0
+            if p.position_x >= screen_width - 20:
+                p.position_x = screen_width - 20
+            if p.position_y <= 0:
+                p.position_y = 0
+            if p.position_y >= screen_height - 20:
+                p.position_y = screen_height - 20
+            
+            # for i in range(len(map.collision_list)):
+            #     if p.position_x < map.collision_list[i][0]:
+            #         p.position_x += p.vel
+            # print(map.collision_list)
+
+            pygame.display.flip()
+        pygame.quit()
 
 if __name__ == "__main__":
     main()
+
+# TO DO:
+# - Finir la fonction de collision
