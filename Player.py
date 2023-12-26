8320ac901d09d03403956d97ad51f14ee1528dd9
Elouen <138658525+Yoshkacafe@users.noreply.github.com>
2023-12-26 16:40:28 +0100

    Update Player.py

--- Player.py
+++ Player.py
@@ -3,23 +3,26 @@ import os
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED_LIGHT = (245, 66, 66)
 
 class Player():
     """
     Spawn a player
     """
     
-    def __init__(self, screen, position_x, position_y):
+    def __init__(self, screen, position_x, position_y, vel):
+        self.screen = screen
         self.position_x = position_x
         self.position_y = position_y
-        self.screen = screen
+        self.vel = vel
+        self.player = pygame.Rect(0, 0, 0, 0)
+        self.life = 3
 
     def draw(self):
-        position_x = 10
-        position_y = 10
-        width = 10
-        height = 10
-        pygame.draw.rect(self.screen, (255,0,0), (position_x, position_y, width, height))
+        width = 15
+        height = 15
+        self.player = pygame.Rect(self.position_x, self.position_y, width, height)
+        pygame.draw.rect(self.screen, RED_LIGHT, self.player)
 
     def top(self):
         self.position_y -= self.vel
@@ -33,32 +36,5 @@ class Player():
     def left(self):
         self.position_x -= self.vel
 
-    def direction(self):
-        x = 50
-        y = 50
-        width = 10
-        height = 10
-        vel = 10
-
-        pygame.time.delay(100)
-        
-    
-
-# class Player:
-
-#     ### Initialisation ###
-#     def __init__(self, row, col):
-#         self.row = row
-#         self.col = col
-
-#     ### Mouvements ###
-
-#     def move(self, dx, dy):
-#         # Ici, vous pouvez mettre votre logique de déplacement du joueur
-#         pass
-
-#     ### Skin Du Joueur ###
-
-#     def draw(self, screen):
-#         # Ici, vous pouvez dessiner le joueur
-#         pass
+    def clear_player(self):
+        pygame.draw.rect(self.screen, WHITE, (self.position_x, self.position_y, 15, 15))
