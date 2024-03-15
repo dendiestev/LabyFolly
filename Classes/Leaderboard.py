49711bf554b4d44ce7390935637b63153efdacb7
Jonas <83239419+Jonas0o0@users.noreply.github.com>
2024-03-15 10:19:54 +0100

    Update Leaderboard.py

--- Classes/Leaderboard.py
+++ Classes/Leaderboard.py
@@ -11,12 +11,20 @@ class Leaderboard():
         self.font2 = pygame.font.SysFont("Times", 20)
         self.font3 = pygame.font.SysFont("Times", 15)
         self.breload = Button(screen=self.screen, pos=(565,550), path_img_up="assets/buttons/reload/up.png", path_img_down="assets/buttons/reload/down.png")
+        self.perso_liste = ["assets/characters/jonesy.png","assets/characters/jonesy_du_bunker.png","assets/characters/jonesy_sombre.png","assets/characters/jonesy_le_noir.png","assets/characters/jonesy_le_lgbtqia2+.png"]
+        self.map_list = ["assets/pp/pp_cod.png","assets/pp/pp_ftn.png","assets/pp/pp_hp.png","assets/pp/pp_mk.png","assets/pp/pp_valo.png"]
 
     def reload(self):
         self.leaderboar = self.bdd.get_all()[:10]
 
     def draw_one(self, v):
         value = self.leaderboar[v]
+
+        perso = pygame.image.load(self.perso_liste[value['character']]).convert_alpha()
+        perso = pygame.transform.scale(perso, (15, 15))
+
+        map = pygame.image.load(self.map_list[value['map']]).convert_alpha()
+        map = pygame.transform.scale(map, (15, 15))
 
         pygame.draw.rect(self.screen, (100,100,100), (200, (45*v)+50, 980, 45))
 
@@ -32,11 +40,9 @@ class Leaderboard():
         p = self.font2.render(f"{value['timer']}", 1, (255,255,255))
         self.screen.blit(p, (675, 45*v+60))
 
-        p = self.font2.render(f"{value['map']}", 1, (255,255,255))
-        self.screen.blit(p, (875, 45*v+60))
+        self.screen.blit(map, (875, 45*v+60))
 
-        p = self.font2.render(f"{value['character']}", 1, (255,255,255))
-        self.screen.blit(p, (1075, 45*v+60))
+        self.screen.blit(perso, (1075, 45*v+60))
 
     def draw_leaderboard(self):
         self.breload.update()
